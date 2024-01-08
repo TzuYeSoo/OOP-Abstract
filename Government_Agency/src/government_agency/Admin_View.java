4596b9d93293184063c947110d5a9bb439258c45
Jessie D. Samaniego <150759974+TzuYeSoo@users.noreply.github.com>
2024-01-08 20:30:45 +0800

    Add files via upload

---
+++ Government_Agency/src/government_agency/Admin_View.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package government_agency;
+
+/**
+ *
+ * @author Josie
+ */
+public class Admin_View extends Admin_Delete{
+    
+    public void AdminViewer(){
+        
+    }
+}
